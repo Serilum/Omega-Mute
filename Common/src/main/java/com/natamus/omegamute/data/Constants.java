a3f45e5c1f9ef7d8fdc87c2a4e3ebecc44b34a2b
Rick South <rick@natamus.com>
2023-12-12 00:39:37 +0100

    Omega Mute source code for 1.19.4.

---
+++ Common/src/main/java/com/natamus/omegamute/data/Constants.java
@@ -0,0 +1,9 @@
+package com.natamus.omegamute.data;
+
+import net.minecraft.client.KeyMapping;
+import net.minecraft.client.Minecraft;
+
+public class Constants {
+	public static KeyMapping hotkey;
+	public static final Minecraft mc = Minecraft.getInstance();
+}
