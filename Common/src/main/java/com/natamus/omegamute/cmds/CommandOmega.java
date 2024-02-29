d14432067f0660255db13fdc80faf903c6d6bab4
Rick South <rick@natamus.com>
2024-02-29 23:14:13 +0100

    (v4.0) The big OmegaMute 4.0 update! [+7]

--- Common/src/main/java/com/natamus/omegamute/cmds/CommandOmega.java
+++ Common/src/main/java/com/natamus/omegamute/cmds/CommandOmega.java
@@ -5,13 +5,14 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.natamus.collective.functions.StringFunctions;
 import com.natamus.omegamute.data.Constants;
-import com.natamus.omegamute.events.MuteEvent;
+import com.natamus.omegamute.data.Variables;
 import com.natamus.omegamute.util.Util;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.world.entity.player.Player;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -22,29 +23,45 @@ public class CommandOmega {
 			.executes((command) -> {
 				return reload();
 			}))
+
 			.then(Commands.literal("query")
 			.executes((command) -> {
 				return query();
 			}))
+
 			.then(Commands.literal("listen").requires((iCommandSender) -> iCommandSender.getEntity() instanceof Player)
 			.executes((command) -> {
-				return listen();
+				return listen(false);
 			}))
+			.then(Commands.literal("listen").requires((iCommandSender) -> iCommandSender.getEntity() instanceof Player)
+			.then(Commands.literal("all")
+			.executes((command) -> {
+				return listen(true);
+			})))
+
 			.then(Commands.literal("mute")
 			.then(Commands.argument("string-contains", StringArgumentType.word())
 			.executes((command) -> {
 				return mute(StringArgumentType.getString(command, "string-contains"));
 			})))
+
 			.then(Commands.literal("cull")
 			.then(Commands.argument("cull-time", IntegerArgumentType.integer(0, 3600))
 			.then(Commands.argument("string-contains", StringArgumentType.word())
 			.executes((command) -> {
 				return cull(StringArgumentType.getString(command, "string-contains"), IntegerArgumentType.getInteger(command, "cull-time"));
 			}))))
+
 			.then(Commands.literal("unmute")
 			.then(Commands.argument("string-contains", StringArgumentType.word())
 			.executes((command) -> {
 				return unmute(StringArgumentType.getString(command, "string-contains"));
+			})))
+
+			.then(Commands.literal("settings")
+			.then(Commands.literal("serilum")
+			.executes((command) -> {
+				return setupSettings("serilum");
 			})))
 		);
 	}
@@ -69,19 +86,20 @@ public class CommandOmega {
 	}
 
 	public static int query() {
-		HashMap<String, Integer> mutedsounds = Util.getMutedSounds();
-		if (mutedsounds.size() > 0) {
+		HashMap<String, Integer> mutedSounds = Util.getMutedSounds();
+		if (mutedSounds.size() > 0) {
 			StringBuilder combined = new StringBuilder();
-			for (String soundname : mutedsounds.keySet()) {
+			for (String soundName : mutedSounds.keySet()) {
 				if (!combined.toString().equals("")) {
 					combined.append(", ");
 				}
-				Integer mutedvalue = mutedsounds.get(soundname);
-				if (mutedvalue > 0) {
-					combined.append(soundname).append("(").append(mutedvalue).append(")");
+
+				Integer mutedValue = mutedSounds.get(soundName);
+				if (mutedValue > 0) {
+					combined.append(soundName).append("(").append(mutedValue).append(")");
 				}
 				else {
-					combined.append(soundname);
+					combined.append(soundName);
 				}
 			}
 
@@ -103,19 +121,30 @@ public class CommandOmega {
 		return 1;
 	}
 
-	public static int listen() {
+	public static int listen(boolean listenToAll) {
 		if (Constants.mc.player == null) {
 			return 1;
 		}
 
-		String playerName = Constants.mc.player.getName().getString();
-		if (MuteEvent.playerIsListening) {
-			MuteEvent.playerIsListening = false;
-			StringFunctions.sendMessage(Constants.mc.player, "You have stopped listening to the active sounds. To toggle it on use '/omegamute listen' again.", ChatFormatting.DARK_GREEN);
+		Variables.listeningToAll = listenToAll;
+		Variables.soundsListenedTo = new ArrayList<String>();
+
+		if (Variables.playerIsListening) {
+			Variables.playerIsListening = false;
+
+			StringFunctions.sendMessage(Constants.mc.player, "You have stopped listening to the active sounds. To toggle it on use '/omegamute listen' again.", ChatFormatting.DARK_GREEN, true);
 		}
 		else {
-			MuteEvent.playerIsListening = true;
-			StringFunctions.sendMessage(Constants.mc.player, "You are now listening to the active sounds. To toggle it off use '/omegamute listen' again.", ChatFormatting.DARK_GREEN);
+			Variables.playerIsListening = true;
+
+			StringFunctions.sendMessage(Constants.mc.player, "You are now listening to the active sounds. To toggle it off use '/omegamute listen' again.", ChatFormatting.DARK_GREEN, true);
+
+			if (listenToAll) {
+				StringFunctions.sendMessage(Constants.mc.player, "Listening to all sounds. To only see sounds once, use '/omegamute listen'", ChatFormatting.GRAY);
+			}
+			else {
+				StringFunctions.sendMessage(Constants.mc.player, "Listening to sounds once. To see all sound occurences, use '/omegamute listen all'", ChatFormatting.GRAY);
+			}
 		}
 
 		return 1;
@@ -172,6 +201,22 @@ public class CommandOmega {
 			StringFunctions.sendMessage(Constants.mc.player, "The soundmap file has been updated.", ChatFormatting.DARK_GREEN);}
 		else {
 			StringFunctions.sendMessage(Constants.mc.player, "No sound events were found by using the wildcard string '" + wildcard + "', try a different query.", ChatFormatting.RED);
+		}
+
+		return 1;
+	}
+
+	public static int setupSettings(String identifier) {
+		if (identifier.equals("serilum")) {
+			cull(".step", 1); // Too many step sounds in an animal farm
+			StringFunctions.sendMessage(Constants.mc.player, " ", ChatFormatting.WHITE);
+			cull(".ambient", 1); // Too many ambient sounds in an animal farm
+			StringFunctions.sendMessage(Constants.mc.player, " ", ChatFormatting.WHITE);
+			mute("entity.cat.ambient");
+			StringFunctions.sendMessage(Constants.mc.player, " ", ChatFormatting.WHITE);
+			mute("entity.cat.stray_ambient"); // Because my IRL cats don't like the meows.
+
+			StringFunctions.sendMessage(Constants.mc.player, "Serilum's favourite Omega Mute settings have been set.", ChatFormatting.GOLD, true);
 		}
 
 		return 1;
