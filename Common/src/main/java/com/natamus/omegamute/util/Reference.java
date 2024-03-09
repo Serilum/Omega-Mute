1f023f008b20ff07a6b7ef578f877083d52d31a0
Rick South <rick@natamus.com>
2024-03-09 13:53:15 +0100

    (v4.1) Updated the mod to use the Collective's improved MessageFunctions instead of StringFunctions.

--- Common/src/main/java/com/natamus/omegamute/util/Reference.java
+++ Common/src/main/java/com/natamus/omegamute/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.omegamute.util;
 public class Reference {
 	public static final String MOD_ID = "omegamute";
 	public static final String NAME = "Omega Mute";
-	public static final String VERSION = "4.0";
+	public static final String VERSION = "4.1";
 	public static final String ACCEPTED_VERSIONS = "[1.20.1]";
 }
